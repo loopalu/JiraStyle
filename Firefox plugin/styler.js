f152b15149d17a4769839228ccd74dc28d625584
Aivar Loopalu <aivar.loopalu@easypark.net>
2021-02-19 21:17:46 +0200

    Firefox extension sets Jira column to green style.

---
+++ Firefox plugin/styler.js
@@ -0,0 +1,8 @@
+var color = "#3bb910";
+
+const columnHeader = document.querySelectorAll("li.ghx-column.ghx-busted.ghx-busted-max.ghx-narrow-card.ghx-xtra-narrow-card")[0];
+columnHeader.style.backgroundColor = color;
+
+const column = document.querySelectorAll("li.adg3 .ghx-column-headers .ghx-column.ghx-busted-max, .adg3 .ghx-columns .ghx-column.ghx-busted-max")[0];
+column.style.backgroundColor = color;
+
