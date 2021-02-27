feca27be207084aa2ff0b066545695be6619701a
Aivar Loopalu <aivar.loopalu@easypark.net>
2021-02-28 00:55:12 +0200

    Made style change persistent. Split the functionality to page functionality and extension button functionality.

---
+++ Firefox extension/extensionButtonFunctionality.js
@@ -0,0 +1,103 @@
+
+const TITLE_APPLY = "Apply style";
+const TITLE_REMOVE = "Remove style";
+const APPLICABLE_PROTOCOLS = ["http:", "https:"];
+let styleIsOn = false;
+
+/*
+Returns true only if the URL's protocol is in APPLICABLE_PROTOCOLS.
+Argument url must be a valid URL string.
+*/
+function protocolIsApplicable(url) {
+    const protocol = (new URL(url)).protocol;
+    return APPLICABLE_PROTOCOLS.includes(protocol);
+}
+
+/*
+Initialize the page action: set icon and title, then show.
+Only operates on tabs whose URL's protocol is applicable.
+*/
+function initializebrowserAction(tab) {
+    if (protocolIsApplicable(tab.url)) {
+        if (styleIsOn) {
+            browser.browserAction.setIcon({tabId: tab.id, path: "icons/letter-j-48-blue.png"});
+            browser.browserAction.setTitle({tabId: tab.id, title: TITLE_REMOVE});
+        } else {
+            browser.browserAction.setIcon({tabId: tab.id, path: "icons/letter-j-48-gray.png"});
+            browser.browserAction.setTitle({tabId: tab.id, title: TITLE_APPLY});
+        }
+        //browser.browserAction.show(tab.id);
+    }
+}
+
+function turnStyleOn(tab) {
+    styleIsOn = true;
+    browser.browserAction.setIcon({tabId: tab.id, path: "icons/letter-j-48-blue.png"});
+    browser.browserAction.setTitle({tabId: tab.id, title: TITLE_REMOVE});
+    notifyModeChange("on");
+}
+
+function turnStyleOff(tab) {
+    styleIsOn = false;
+    browser.browserAction.setIcon({tabId: tab.id, path: "icons/letter-j-48-gray.png"});
+    browser.browserAction.setTitle({tabId: tab.id, title: TITLE_APPLY});
+    notifyModeChange("off");
+}
+
+function toggleStyle (tab) {
+    function gotTitle(title) {
+        if (styleIsOn) {
+            turnStyleOff(tab)
+        } else {
+            turnStyleOn(tab)
+        }
+    }
+
+    const gettingTitle = browser.browserAction.getTitle({tabId: tab.id});
+    gettingTitle.then(gotTitle);
+}
+
+function notifyModeChange (newMode) {
+    browser.storage.local.set({ "JiraStyle": newMode });
+    browser.tabs.query({}, function(tabs) {
+        for (let i=0; i<tabs.length; ++i) {
+            const tab = tabs[i];
+            const url = tab.url;
+            if (url.indexOf("jira.com") !== -1) {
+                browser.tabs.sendMessage(tab.id, {action: newMode});
+            }
+        }
+    });
+}
+
+browser.storage.local.get(["JiraStyle"], function(items){
+    if (items["JiraStyle"] === 'off') {
+        styleIsOn = false;
+        //setLightProperties();
+    } else {
+        styleIsOn = true;
+        //setDarkProperties();
+    }
+});
+
+/*
+Toggle CSS when the page action is clicked.
+*/
+browser.browserAction.onClicked.addListener(toggleStyle);
+
+/*
+When first loaded, initialize the page action for all tabs.
+*/
+const gettingAllTabs = browser.tabs.query({});
+gettingAllTabs.then((tabs) => {
+    for (let tab of tabs) {
+        initializebrowserAction(tab);
+    }
+});
+
+/*
+Each time a tab is updated, reset the page action for that tab.
+*/
+browser.tabs.onUpdated.addListener((id, changeInfo, tab) => {
+    initializebrowserAction(tab);
+});
---
+++ Firefox extension/pageFunctionality.js
@@ -0,0 +1,36 @@
+// Initial injection - checks if dark mode is enabled.
+browser.storage.local.get(["JiraStyle"], injectOnLoad);
+
+function injectOnLoad(items){
+    if (items["JiraStyle"] !== 'off') {
+        (document.body || document.head || document.documentElement).appendChild(style);
+    }
+}
+
+// Subscribe to other necessary events.
+document.addEventListener('DOMContentLoaded', changeStyleImportance, false);
+browser.runtime.onMessage.addListener(toggleStyle);
+
+// Moves style tag to the head once the document has loaded.
+function changeStyleImportance(){
+    document.removeEventListener('DOMContentLoaded', changeStyleImportance, false);
+    document.getElementsByTagName('head')[0].appendChild(document.getElementById('JiraStyle'));
+}
+
+// Toggle style on/off when toggle is activated.
+function toggleStyle(message, sender, sendResponse) {
+    if (message.action === 'on') {
+        document.getElementsByTagName("head")[0].appendChild(style);
+        location.reload();
+    }
+    else if (message.action === 'off') {
+        var element = document.getElementById('JiraStyle');
+        element.parentElement.removeChild(element);
+    }
+}
+
+var style = document.createElement('style');
+style.id = "JiraStyle";
+style.className = "JiraStyle";
+style.type = "text/css";
+style.textContent = `.adg3 .ghx-column-headers .ghx-column.ghx-busted-max, .adg3 .ghx-columns .ghx-column.ghx-busted-max { background: #3bb910; }`;
